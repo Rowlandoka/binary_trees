15bab5f50fec20be8a99baa8751aaf8944d80b61
Rowland Oka <rowlandoka@gmail.com>
2023-03-04 05:31:39 +0100

    binary tree tasks

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,14 +4,15 @@
  * @tree: node to start
  * Return: true or false
  */
- int binary_tree_is_full(const binary_tree_t *tree)
- {
-    if (tree == NULL)
-        return (0);
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
-    if ((tree->left) && (tree->right))
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    
-    return (0);
- }
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+if ((tree->left) && (tree->right))
+return (binary_tree_is_full(tree->left) &&
+binary_tree_is_full(tree->right));
+
+return (0);
+}
